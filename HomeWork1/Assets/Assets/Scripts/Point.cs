021bd8ea18a4a659eff151a0e809c43b9b4502d2
junhyub <junhyub3090@gmail.com>
2017-12-07 12:51:25 +0900

    Fixed-1

---
+++ HomeWork1/Assets/Assets/Scripts/Point.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb2D1;
+    private float time;
+   
+    float xMove, yMove;
+   
+
+    void Start()
+    {
+        /*rb2D1 = GetComponent<Rigidbody2D>();
+        rb2D1.velocity = transform.forward * speed;*/
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            xMove = speed * Time.deltaTime;
+            yMove = 0;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            xMove = -speed * Time.deltaTime;
+            yMove = 0;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            yMove = speed * Time.deltaTime;
+            xMove = 0;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            yMove = -speed * Time.deltaTime;
+            xMove = 0;
+        }
+        while(time>=0)
+        {
+            if (time >= 1)
+            {
+                this.transform.Translate(new Vector2(xMove, yMove));
+                time = time - 1;
+            }
+        }
+
+
+    }
+}
+
+
+     
---
+++ HomeWork1/Assets/Assets/Scripts/Pointer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pointer : MonoBehaviour
+{
+    public GameObject Sprite;
+
+
+
+    void Start()
+    {
+
+       
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 touchPos = new Vector2(worldPos.x, worldPos.y);
+
+            transform.position = touchPos;
+
+            GameObject Clone = Instantiate(Sprite, transform.position, Quaternion.identity) as GameObject;
+
+        }
+    }
+
+
+}

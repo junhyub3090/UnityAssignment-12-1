64ee72237a190eb3ee9347f79c0d2fd65376404f
junhyub <junhyub3090@gmail.com>
2017-12-08 13:21:20 +0900

    Fixed-4

--- HomeWork1/Assets/Assets/Scripts/Point.cs
+++ HomeWork1/Assets/Assets/Scripts/Point.cs
@@ -3,48 +3,42 @@ using System.Collections;
 
 public class Point : MonoBehaviour
 {
-    public float speed;
-    Rigidbody2D rb2D1;
+    public float timeGap;
     private float time=0;
-   
-    float xMove, yMove;
-   
 
-    void Start()
-    {
-        /*rb2D1 = GetComponent<Rigidbody2D>();
-        rb2D1.velocity = transform.forward * speed;*/
-    }
+    float xMove, yMove;
+
     private void Update()
     {
+        float width = gameObject.GetComponent<Renderer>().bounds.size.x;
         time += Time.deltaTime;
 
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            xMove = speed;
+            xMove = width;
             yMove = 0;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            xMove = -speed;
+            xMove = -width;
             yMove = 0;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            yMove = speed;
+            yMove = width;
             xMove = 0;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            yMove = -speed;
+            yMove = -width;
             xMove = 0;
         }
         
-            if (time >= 0.2f)
+            if (time >= timeGap)
             {
                 this.transform.Translate(new Vector2(xMove, yMove));
-                time = time - 0.2f;
+                time = time - timeGap;
             }
         
 

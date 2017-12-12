622177e28fcdbe321f756488181b431c221f9725
junhyub <junhyub3090@gmail.com>
2017-12-12 12:09:51 +0900

    FollowingSprite

---
+++ 12월 3주차 과제/Assets/Asset/Scripts/PlayerPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPosition : MonoBehaviour {
+    public GameObject Sprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+        Sprite.transform.position = worldPos;
+
+    }
+}

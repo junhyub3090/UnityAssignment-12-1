666ff2205c763c89bb8c029055a93955586e9b61
junhyub <junhyub3090@gmail.com>
2017-12-12 21:41:31 +0900

    blocked1

---
+++ 12월 3주차 과제/Assets/Asset/Scripts/BulletMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+    public GameObject player;
+    public GameObject target;
+    public float speed;
+    private Transform bulletHead;
+    private Transform bulletTail;
+    private Transform distance;
+
+
+	// Use this for initialization
+	void Start () {
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        Vector3 distance = target.transform.position - player.transform.position;
+        Vector3 PlayerTargetDirection = distance.normalized;
+        bulletTail.position = player.transform.position;
+        bulletHead.position = player.transform.position + 2 * PlayerTargetDirection;
+        bulletTail.position += PlayerTargetDirection * speed * Time.deltaTime;
+        lineRenderer.SetPosition(0,bulletTail.position);
+        lineRenderer.SetPosition(1,bulletHead.position);
+
+    }
+}
---
+++ 12월 3주차 과제/Assets/Asset/Scripts/CallBullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallBullet : MonoBehaviour
+{
+    public GameObject LineRenderer;
+
+
+
+    void Start()
+    {
+
+
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPos.z = 0;
+            Instantiate(LineRenderer, worldPos, Quaternion.identity);
+            
+
+        }
+    }
+
+
+}
